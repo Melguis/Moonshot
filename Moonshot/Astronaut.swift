09082b49072d4f40b055aaa660d1c37835148b9b
Jorge Henrique <jorgehenriquedasilva2014@hotmail.com>
2023-06-12 20:35:19 -0300

    First commit

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Jorge Henrique on 12/06/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
